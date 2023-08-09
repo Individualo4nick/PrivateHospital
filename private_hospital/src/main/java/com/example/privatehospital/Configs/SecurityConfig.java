8c82ed6128c8bce662835d22112fb39caa931b3e
Vitaliy <modecom.pirogenko@gmail.com>
2023-08-09 15:52:09 +0300

    add cors configuration

---
+++ private_hospital/src/main/java/com/example/privatehospital/Configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.privatehospital.Configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebMvcConfigurer {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests(requst -> requst
+                        .anyRequest().permitAll()).build();
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("*");
+    }
+}
